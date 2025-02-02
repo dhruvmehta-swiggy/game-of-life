7ec3bbc53f14b5c933ff8fd38c9871ef57a6044a
dhruvmehta-swiggy <dhruv.mehta_ftc@external.swiggy.in>
2025-02-02 16:40:01 +0530

    Add Cell

---
+++ src/main/java/com/swiggy/game_of_life/Cell.java
@@ -0,0 +1,10 @@
+package com.swiggy.game_of_life;
+
+public class Cell {
+
+    private boolean isAlive;
+
+    public Cell(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+}
---
+++ src/test/java/com/swiggy/game_of_life/CellTest.java
@@ -0,0 +1,7 @@
+package com.swiggy.game_of_life;
+
+public class CellTest {
+
+
+
+}
